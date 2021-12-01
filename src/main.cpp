a162d8f8caee1eb632e770facf20d10f6d8ee391
mahdi <ssmmhh1382@gmail.com>
2021-12-01 19:27:40 +0330

    convert 10base number to binary

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
-
-int main(int argc, char** argv) {
+//a template function for converting int to string
+template <typename T>
+  std::string NumberToString ( T Number )
+  {
+     std::ostringstream ss;
+     ss << Number;
+     return ss.str();
+  }
+  
+int main() {
 	
-	cout << "23/2? "<< 23/2;
+	int number;
+	cout << "inter a number: ";
+	cin >> number;
+	
+	//an int to store kharjeGhesmat
+	int kharjGhesmat ,baghimande;
+	
+	//a string to store final result
+	string javab = "" ;
+	do{
+		kharjGhesmat = number/2;
+		baghimande = (number%2);
+	  
+		javab = NumberToString(baghimande) + javab;
+		number = number/2;
+	}
+	while(kharjGhesmat>0);
+	
+	cout << "javab: " << javab;
+	
 	return 0;
 }
+
