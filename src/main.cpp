ab811806ba6f1cdbf756d5b1545bdb26beb0541d
mahdi <ssmmhh1382@gmail.com>
2021-12-04 12:11:45 +0330

    test for loops in cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,12 @@
 #include <sstream>
 
 using namespace std;
-//a template function for converting int to string
-template <typename T>
-  std::string NumberToString ( T Number )
-  {
-     std::ostringstream ss;
-     ss << Number;
-     return ss.str();
-  }
   
 int main() {
 	
-	int number;
-	cout << "inter a number: ";
-	cin >> number;
-	
-	//an int to store kharjeGhesmat
-	int kharjGhesmat ,baghimande;
-	
-	//a string to store final result
-	string javab = "" ;
-	do{
-		kharjGhesmat = number/2;
-		baghimande = (number%2);
-	  
-		javab = NumberToString(baghimande) + javab;
-		number = number/2;
+	for(int i = 0;i<5;i++){
+		cout<<i<<endl;
 	}
-	while(kharjGhesmat>0);
-	
-	cout << "javab: " << javab;
 	
 	return 0;
 }
