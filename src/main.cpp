4327ee173d8326bf23a9f6b20a9ce8a1bc7c0099
mahdi <ssmmhh1382@gmail.com>
2021-12-06 23:44:02 +0330

    add binary search algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,40 @@ int linearSearch(int arr[], int lenght, int query);
 /* Function to sort an array using bubbleSortt algorithm*/
 void bubbleSort(int arr[], int n);
 
+/* Function to search in array using binarySearch algorithm */
+int binarySearch(int arr[], int lenght, int query);
+
 /* Function to print an array */
 void printArray(int arr[], int lenght);
 
+/* Main function*/
 int main() {
-  int arr[] = {22, 44, 66, 88, 77, 55, 11};
-  // int query;
-  // cout << "Inter a search query: ";
-  // cin >> query;
-  // int result = linearSearch(arr, 7, query);
-  //
-  // cout << result << endl;
+
+  // test array and size of test array
+  int arr[] = {22, 44, 66, 88, 77, 44, 55, 11, 33};
   int lenght = sizeof(arr) / sizeof(arr[0]);
+
+  // get search query from user
+  int query;
+  cout << "Inter a search query: ";
+  cin >> query;
+
+  // print raw array
+  cout << "array:       ";
   printArray(arr, lenght);
+
+  // find query using linearSearch
+  cout << "linearSearch: " << linearSearch(arr, 7, query) << endl;
+
+  // sort arry
   bubbleSort(arr, lenght);
-  cout << "sort.." << endl;
+
+  // print sorted array
+  cout << "sortedArray: ";
   printArray(arr, lenght);
+
+  // find query using binarySearch
+  cout << "binarySearch: " << binarySearch(arr, 7, query) << endl;
 }
 
 int linearSearch(int arr[], int lenght, int query) {
@@ -32,22 +50,54 @@ int linearSearch(int arr[], int lenght, int query) {
 
     if (arr[i] == query) {
 
-      cout << "We found your query in index: " << i << endl;
+      cout << "ls: There is a object matching query in index: " << i << endl;
       return arr[i];
     }
   }
   // there isn't any object in arr[] matching query
-  cout << "We search whole array and didn't find any matching result" << endl;
+  cout << "ls: There isn't any object in array matching with entered query"
+       << endl;
   return query;
 }
 
 void bubbleSort(int arr[], int n) {
   for (int i = 1; i < n; i++)
-    // bubble up max{a[0..n-i]}:
+
     for (int j = 0; j < n - i; j++)
+
       if (arr[j] > arr[j + 1])
         swap(arr[j], arr[j + 1]);
-  // INVARIANT: a[n-1-i..n-1] is sorte
+}
+
+int binarySearch(int arr[], int lenght, int query) {
+  // array in binary search should be sorted first
+  bubbleSort(arr, lenght);
+  int low = 0;
+  int high = lenght - 1;
+  int mid;
+
+  while (low <= high) {
+    // the middle of low and high
+    mid = (low + high) / 2;
+
+    // if the number in the middle is equal to query return it
+    if (arr[mid] == query) {
+      cout << "bs: There is a object matching query in index: " << mid << endl;
+      return arr[mid];
+    }
+    if (arr[mid] < query)
+      // if the number in the middle is less then query it means
+      // query isn't in first half so the new low is 'mid + 1';
+      low = mid + 1;
+    else
+      // if the number in the middle is greater then query it means
+      // query isn't in the second half so the new hight is 'mid - 1'
+      high = mid - 1;
+  }
+  // there isn't any object in arr[] matching with entered query
+  cout << "bs: There isn't any object in array matching with entered query"
+       << endl;
+  return query;
 }
 
 void printArray(int arr[], int lenght) {
