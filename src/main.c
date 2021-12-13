93c1731e4e1f29faafb765bca29f4df5702706bc
mahdi <ssmmhh1382@gmail.com>
2021-12-13 21:58:41 +0330

    calculate signal

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <math.h>
-
-using namespace std;
+#include <stdio.h>
 
 int main() {
 
@@ -10,9 +8,9 @@ int main() {
   for (int k = 1; k <= 4; k++) {
     for (int n = 0; n <= 6; n++) {
       float result = sin(((PI / k) * n));
-      cout << result << " | ";
+      printf("%5.2f | ", result);
     }
-    cout << endl;
+    printf("\n");
   }
 
   return 0;
