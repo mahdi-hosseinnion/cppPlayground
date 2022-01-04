60f732ee9191e836336f7f105a63006eb88cf72d
mahdi <ssmmhh1382@gmail.com>
2022-01-04 12:02:35 +0330

    Fix gavassi

--- src/main.c
+++ src/main.c
@@ -2,20 +2,15 @@
 #include <stdio.h>
 
 int gaussian(float A[][8], int n, float c[]) {
-  int u = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = (n - 1); j > i; j--) {
-      if (i >= 1) {
-        u++;
-      }
-      float zarib = -1 * ((A[j][i]) / (A[u][i]));
-
+  for (int j = 0; j < n; j++) {
+    for (int i = (n - 1); i > j; i--) {
+      float zarib = -1 * ((A[i][j]) / (A[j][j]));
       for (int w = 0; w < n; w++) {
-        float zarbShodeDarZarib = zarib * A[u][w];
-        A[j][w] = A[j][w] + zarbShodeDarZarib;
+        float zarbShodeDarZarib = zarib * A[j][w];
+        A[i][w] = A[i][w] + zarbShodeDarZarib;
       }
-      float zarbShodeDarZarib = zarib * c[u];
-      c[j] = c[j] + zarbShodeDarZarib;
+      float zarbShodeDarZarib = zarib * c[j];
+      c[i] = c[i] + zarbShodeDarZarib;
     }
   }
 }
@@ -31,19 +26,26 @@ int printXs(float A[][8], int n, float c[]) {
     float xn = (c[i] + sum) / A[i][i];
     answerList[i] = xn;
   }
-  for (int i = n; i > 0; i--) {
-    printf("x#%d: %1.1f \n", i, answerList[(i - 1)]);
+  for (int i = 0; i < n; i++) {
+    printf("x#%d: %2.2f \n", (i + 1), answerList[i]);
   }
 }
 
 int main() {
   // n should be smaller then 9
-  int n = 3;
-  float c[n] = {0, 4, 2};
-  float arr[n][8] = {{1, 1, 1}, {1, -2, 2}, {1, 2, -1}};
-
+  printf("4*4\n");
+  int n = 4;
+  float c[n] = {2, -3, 5, -1};
+  float arr[n][8] = {
+      {2, 5, 3, -3}, {-2, -2, 3, 0}, {1, 2, 3, 4}, {-5, -4, -3, -2}};
   gaussian(arr, n, c);
   printXs(arr, n, c);
+  printf("3*3\n");
+  n = 3;
+  float cc[n] = {0, 4, 2};
+  float arrr[n][8] = {{1, 1, 1}, {1, -2, 2}, {1, 2, -1}};
+  gaussian(arrr, n, cc);
+  printXs(arrr, n, cc);
 
   return 0;
 }
--- src/temp.c
+++ src/temp.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 int main() {
-  int i, j, k, n;
-  float A[20][20], c, x[10];
-  printf("\nEnter the size of matrix: ");
-  scanf("%d", &n);
-  printf("\nEnter the elements of augmented matrix row-wise:\n");
-  for (i = 1; i <= n; i++) {
-    for (j = 1; j <= (n + 1); j++) {
-      printf(" A[%d][%d]:", i, j);
-      scanf("%f", &A[i][j]);
-    }
-  }
+  int n = 4;
+  float x[10] = {2, -3, 5, -1};
+  float A[20][20] = {
+      {2, 5, 3, -3}, {-2, -2, 3, 0}, {1, 2, 3, 4}, {-5, -4, -3, -2}};
+  int i, j, k; //, n;
+  float c;
+  // float A[20][20], c, x[10];
+  // printf("\nEnter the size of matrix: ");
+  // scanf_s("%d", &n);
+  // printf("\nEnter the elements of augmented matrix row-wise:\n");
+  // for (i = 1; i <= n; i++) {
+  //   for (j = 1; j <= (n + 1); j++) {
+  //     printf(" A[%d][%d]:", i, j);
+  //     scanf_s("%f", &A[i][j]);
+  //   }
+  // }
   /* Now finding the elements of diagonal matrix */
-  for (j = 1; j <= n; j++) {
-    for (i = 1; i <= n; i++) {
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
       if (i != j) {
-        c = A[i][j] / A[j][j];
+        c = A[j][i] / A[j][j];
         for (k = 1; k <= n + 1; k++) {
-          A[i][k] = A[i][k] - c * A[j][k];
+          A[k][i] = A[k][i] - c * A[k][j];
         }
       }
     }
   }
   printf("\nThe solution is:\n");
   for (i = 1; i <= n; i++) {
-    x[i] = A[i][n + 1] / A[i][i];
+    x[i] = A[n + 1][i] / A[i][i];
     printf("\n x%d=%f\n", i, x[i]);
   }
   return (0);
