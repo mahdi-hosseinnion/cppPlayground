83a709fe47e9b1ee9b47e6ffe9e33324214aa291
mahdi <ssmmhh1382@gmail.com>
2022-01-14 00:06:27 +0330

    mYstrstr

--- src/main.c
+++ src/main.c
@@ -1,26 +1,41 @@
+#include "assert.h"
 #include "stdio.h"
+#include "string.h"
 
-const int size = 100;
+char *MYstrstr(char *str1, char *str2) {
+  while (*str1 != '\0') {
 
-void convertAtoaAndReverse(char string[]) {
-
-  for (int i = 0; i < size; i++) {
-    char temp = string[i];
-    if (temp >= 'a' && temp <= 'z') {
-      temp -= ('a' - 'A');
-    } else if (temp >= 'A' && temp <= 'z') {
-      temp += ('a' - 'A');
+    if (*str1 == *str2) {
+      char *temp1 = str1;
+      char *temp2 = str2;
+      bool didFounded = true;
+      while (*temp1 != '\0' && *temp2 != '\0') {
+        if (*temp1 != *temp2) {
+          didFounded = false;
+          break;
+        }
+        temp1++;
+        temp2++;
+      }
+      if (didFounded) {
+        return str1;
+      }
     }
-    string[i] = temp;
+    str1++;
   }
+
+  return NULL;
 }
 int main() {
 
-  char string[size];
-  printf("Inter a string: (maximum lenght = %d)", size);
-  scanf("%s", string);
-  convertAtoaAndReverse(string);
-  printf("Result is: %s", string);
-
+  char str[] = "GeekForTheBestSham";
+  char str2[] = "The";
+  char *sub;
+  char *sub2;
+  sub = strstr(str, str2);
+  sub2 = MYstrstr(str, str2);
+  printf("\nSubstring  is: |%s|", sub);
+  printf("\n\nSubstring2 is: |%s|", sub2);
+  assert(sub == sub2);
   return 0;
 }
